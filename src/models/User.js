d17f39b978b631fddfae5a11a9c690935aee7bd6
Ivan Gillig <ivan.gillig@gmail.com>
2024-06-07 00:52:14 -0300

    user model adjusted

--- src/models/User.js
+++ src/models/User.js
@@ -5,15 +5,46 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  displayName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  profilePhoto: {
+    type: String,
+  },
+  // TODO: Check if other fields are neccesary
 });
 
 userSchema.statics.findOrCreate = async function (profile) {
-  let user = await this.findOne({ googleId: profile.id });
-  if (!user) {
-    user = new this({ googleId: profile.id });
-    await user.save();
+  try {
+    let user = await this.findOne({ googleId: profile.id });
+    const displayName = profile.displayName || '';
+    const email = profile.emails && profile.emails[0] ? profile.emails[0].value : '';
+    const profilePhoto = profile.photos && profile.photos[0] ? profile.photos[0].value : '';
+
+    if (user) {
+      // Update user if exists
+      user.displayName = displayName;
+      user.email = email;
+      user.profilePhoto = profilePhoto;
+      // Add fields if neccessary
+      await user.save();
+    } else {
+      user = new this({
+        googleId: profile.id,
+        displayName: displayName,
+        email: email,
+        profilePhoto: profilePhoto,
+      });
+      await user.save();
+    }
+    return user;
+  } catch (err) {
+    throw err;
   }
-  return user;
 };
 
 const User = mongoose.model('User', userSchema);
