7d1df7142bb48a2c7e9ba64ed5b4c2a505497a33
Ivan Gillig <ivan.gillig@gmail.com>
2024-08-12 20:26:42 -0300

    adjusts on login proccess

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,8 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  scope: ["profile", "email"]
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const user = await User.findOrCreate(profile);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,7 @@ import express from 'express';
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import User from '../models/User.js'
 
 dotenv.config();
 
@@ -16,9 +17,9 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   (req, res) => {
-    const token = jwt.sign({ id: req.user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    res.redirect(`${process.env.FRONTEND_URL}/auth/callback?token=${token}&user=${encodeURIComponent(JSON.stringify(req.user))}`);
+    res.redirect(`${process.env.FRONTEND_URL}/auth/callback?token=${token}`);
   }
 );
 
@@ -30,5 +31,40 @@ router.post('/logout', (req, res) => {
     res.status(200).json({ message: 'Logout successful' });
   });
 });
+
+// @desc    Obtener la información completa del usuario
+// @route   GET /auth/getUserInfo
+router.get('/getUserInfo', authenticateToken, async (req, res) => {
+  try {
+    // Ensure the decoded JWT contains a user ID
+    if (!req.user || !req.user.id) {
+      return res.status(400).json({ message: 'Invalid token or user ID missing' });
+    }
+
+    // Find user by ID (decoded from JWT)
+    const user = await User.findById(req.user.id).select('displayName email profilePhoto');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error('Error retrieving user data:', error);
+    res.status(500).json({ message: 'Error retrieving user data', error: error.message });
+  }
+});
+
+// Middleware
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 export default router;
